3e3d581b9cd0295d5aa5def3a3415d52b33d7933
Robert <robertableson@gmail.com>
2020-03-15 12:30:24 -0400

    Added API endpoint to add new static employee.

--- functions/index.js
+++ functions/index.js
@@ -29,6 +29,47 @@ app.get('/employees', (request, response) => {
   });
 });
 
+app.post('/employee', (request, response) => { 
+  let employee = {
+    firstName: 'Rob',
+    lastName: 'Paul',
+    hourlyWage: 343,
+    birthDate: '1973-03-19T00:00Z'
+  };
+
+  //response.send(employee);  
+  
+  db.collection('employees').add(employee).then(ref => {
+    console.log('Added document with ID: ', ref.id);
+    response.send({
+      'status': 'success',
+      'docId': ref.id
+    });
+  }).catch((err) => {
+    console.log('Error adding document', err);
+    response.send({
+      'status': 'fail',
+      'message': err
+    });
+  });
+  
+  
+  /*db.collection('employees').get()
+  .then((snapshot) => {
+    let employees = [];
+    
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+      employees.push(doc.data());
+    });
+
+    response.send(employees);
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });*/
+});
+
 
 
 exports.app = functions.https.onRequest(app);
