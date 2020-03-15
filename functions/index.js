2047e5d7d922d0371dd24633d18c843b4c37588a
Robert <robertableson@gmail.com>
2020-03-15 12:07:22 -0400

    Added API endpoint to get all employees in json format.

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,34 @@
 const functions = require('firebase-functions');
 const express = require('express');
+var admin = require("firebase-admin");
 
+var serviceAccount = require("./employee-management-15524-firebase-adminsdk-krnni-5e09b0fc09.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://employee-management-15524.firebaseio.com"
+});
+
+const db = admin.firestore();
 const app = express();
 
-let employees = [
-  {  
-    'firstName': 'Marry',
-    'lastName': 'Lovin',
-    'hourlyWage': 20,
-    'birthDate': '1993-03-19T00:00Z'
-  },
-  {  
-    'firstName': 'Harry',
-    'lastName': 'McBurger',
-    'hourlyWage': 40,
-    'birthDate': '1983-03-19T00:00Z'
-  }
-];
-
 app.get('/employees', (request, response) => {
-  response.send(employees);
+  db.collection('employees').get()
+  .then((snapshot) => {
+    let employees = [];
+    
+    snapshot.forEach((doc) => {
+      console.log(doc.data());
+      employees.push(doc.data());
+    });
+
+    response.send(employees);
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
 });
+
+
 
 exports.app = functions.https.onRequest(app);
