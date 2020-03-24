7409fd5176102532cf1c8be64d540581bc30b7f2
Robert <robertableson@gmail.com>
2020-03-23 20:58:16 -0400

    Added basic table look

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    Table here
+  <div class="table-template">
+    <table>
+      <tr>
+        <th class="checkbox-col"></th>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Birth date</th>
+        <th>Hourly wage</th>
+      </tr>
+
+      <tr>
+        <td class="checkbox-col">
+          <input type="checkbox">
+        </td>
+        <td>prénom</td>
+        <td>nom</td>
+        <td>borth date</td>
+        <td>46$</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -9,5 +27,30 @@ export default {
   name: 'EmployeeTable'
 }
 </script>
+
+<style>
+  table{
+    width: 100%;    
+  }
+  td, th{
+    border: 1px solid #ddd;
+    text-align: left;
+    padding: 8px;
+  }
+
+  table{
+    border-collapse: collapse;
+  }
+
+  .table-template{
+    padding: 20px 0;
+  }
+
+  .checkbox-col{
+    border:none;
+    text-align: right;
+    width: 12px;
+  }
+</style>
 
 
--- src/components/EmployeeTableActions.vue
+++ src/components/EmployeeTableActions.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    Table actions here
+  <div class="table-actions-template">
+    <a class="table-action table-action-add-new button button-add" href="#">Add new</a>
+    <a class="table-action table-action-edit button button-edit" href="#">Edit</a>
+    <a class="table-action table-action-add-delete button button-delete" href="#">Delete</a>
   </div>
 </template>
 
@@ -10,4 +12,31 @@ export default {
 }
 </script>
 
+<style scoped>
+  a.button{
+    text-decoration: none;
+    padding: 5px 10px;
+  }
+  .table-actions-template{
+    text-align: right;
+  }
+  a.button-add{
+    background-color: rgb(88, 199, 88);
+    color: #fff;
+    text-decoration: none;
+  }
+    a.button-edit{
+    background-color: rgb(199, 147, 88);
+    color: #fff;
+    margin-left: 10px;
+  }
+  a.button-delete{
+    background-color: rgb(199, 88, 88);
+    color: #fff;
+    margin-left: 10px;
+  }
+  a.table-action{
+    display: inline;
+  }
+</style>
 
