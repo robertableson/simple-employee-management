16e3d1d5973711e764cf4d3413e9a01eefe0f3e3
Robert <robertableson@gmail.com>
2020-03-29 14:14:48 -0400

    Can now modify an employee in DB.

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,6 @@ export default {
       axios.get('http://localhost:5000/employees')
         .then((response) => {
           this.employees = response.data;
-          console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
@@ -45,7 +44,12 @@ export default {
       }
       else if(action == 'edit') {
         let id = this.getSelectedEmployeeIds();
-        this.editSelectedEmployee(id);
+        if(id.length > 1 || id.length < 1) {
+          alert('SVP sélectionner un seul employé pour pouvoir l\'éditer.');
+        }
+        else {
+          this.editSelectedEmployee(id);
+        }
       }
       else if(action == 'delete') {
         let ids = this.getSelectedEmployeeIds();
@@ -53,15 +57,23 @@ export default {
       }
     },
     addNewEmployee() {
+      let that = this;
+
       Swal.fire({
         title: 'Add new',
+        onOpen: function() {
+          document.querySelector('form#add-new-employee').addEventListener('submit', function(e){
+            e.preventDefault();
+            that.addNewEmployeeFormSent();
+          });          
+        },
         showCancelButton: false,
         showConfirmButton:false,
         customClass: {
-          container: 'add-employee-modal'
+          container: 'employee-modal'
         },
         html: `
-          <form id="add-new-employee">
+          <form action="http://localhost:5000/employee" method="post" id="add-new-employee">
             <label for="first-name">First name</label>
             <br/>
             <input id="first-name" type="text"/>
@@ -91,41 +103,139 @@ export default {
             <button>Cancel</button>
           </form>
         `
-      }).then((result) => {
-        if (result.value) {
-          let newEmployee = this.getEmployeeToAddData();
-          let newEmployeeQueryString = 
-            'firstName=' + newEmployee.firstName +
-            '&lastName=' + newEmployee.lastName +
-            '&birthDate=' + newEmployee.birthDate +
-            '&hourlyWage=' + newEmployee.hourlyWage
-          ;
-
-          axios.post('http://localhost:5000/employee?' + newEmployeeQueryString)
-            .then((response) => {
-              console.log(response);
-              this.firebase_fetchAllEmployees();
-            })
-            .catch((error) => {
-              console.log(error);
-            }
-          );
-        }
       });
     },
-    getEmployeeToAddData() {
+    addNewEmployeeFormSent() {
+      let newEmployee = this.getEmployeeModalData();
+      let newEmployeeQueryString = 
+        'firstName=' + newEmployee.firstName +
+        '&lastName=' + newEmployee.lastName +
+        '&birthDate=' + newEmployee.birthDate +
+        '&hourlyWage=' + newEmployee.hourlyWage
+      ;        
+
+      axios.post('http://localhost:5000/employee?' + newEmployeeQueryString)
+        .then((response) => {
+          console.log(response);
+          this.firebase_fetchAllEmployees();
+        })
+        .catch((error) => {
+          console.log(error);
+        }
+      );
+
+      Swal.close();
+    },
+    editEmployeeFormSent(_id) {
+      let editedEmployee = this.getEmployeeModalData();
+      let editedEmployeeQueryString = 
+        'firstName=' + editedEmployee.firstName +
+        '&lastName=' + editedEmployee.lastName +
+        '&birthDate=' + editedEmployee.birthDate +
+        '&hourlyWage=' + editedEmployee.hourlyWage
+      ;        
+
+      console.log('http://localhost:5000/employee/' + _id + '?' + editedEmployeeQueryString);
+      
+
+      axios.put('http://localhost:5000/employee/' + _id + '?' + editedEmployeeQueryString)
+        .then((response) => {
+          console.log(response);
+          this.firebase_fetchAllEmployees();
+        })
+        .catch((error) => {
+          console.log(error);
+        }
+      );
+
+      Swal.close();
+    },
+    getEmployeeModalData() {
+      const input_employeeFirstName = document.querySelector('.employee-modal form input#first-name');
+      const input_employeeLastName = document.querySelector('.employee-modal form input#last-name');
+      const input_employeeBirthDate = document.querySelector('.employee-modal form input#birth-date');
+      const input_employeeHourlyWage = document.querySelector('.employee-modal form input#hourly-wage');
+
       let newEmployee = {
-        firstName: 'test',
-        lastName: 'test',
-        birthDate: 'test',
-        hourlyWage: 11
+        firstName: input_employeeFirstName.value,
+        lastName: input_employeeLastName.value,
+        birthDate: input_employeeBirthDate.value,
+        hourlyWage: input_employeeHourlyWage.value
       };
 
       return newEmployee;
     },
-    editSelectedEmployee(id) {
-      console.log(id);      
-      console.log('Edit Clicked');
+    editSelectedEmployee(_id) {
+      let employeeToEdit = this.getEmployeeDataById(_id);
+
+      let that = this;
+
+      Swal.fire({
+        title: 'Edit',
+        onOpen: function() {
+          document.querySelector('form#edit-employee').addEventListener('submit', function(e){
+            e.preventDefault();
+            that.editEmployeeFormSent(_id);
+          });          
+        },
+        showCancelButton: false,
+        showConfirmButton:false,
+        customClass: {
+          container: 'employee-modal'
+        },
+        html: `
+          <form method="post" id="edit-employee">
+            <label for="first-name">First name</label>
+            <br/>
+            <input id="first-name" type="text" value="${employeeToEdit.data.firstName}"/>
+
+            <br/>
+            <br/>
+
+            <label for="last-name">Last name</label>
+            <br/>
+            <input id="last-name" type="text" value="${employeeToEdit.data.lastName}"/>
+
+            <br/>
+            <br/>
+
+            <label for="birth-date">Birth date</label>
+            <br/>
+            <input id="birth-date" type="text" value="${employeeToEdit.data.birthDate}"/>
+
+            <br/>
+            <br/>
+
+            <label for="hourly-wage">Hourly wage</label>
+            <br/>
+            <input id="hourly-wage" type="text" value="${employeeToEdit.data.hourlyWage}"/>
+
+            <input type="submit" value="Add"/>
+            <button>Cancel</button>
+          </form>
+        `
+      });
+      
+    },
+    getEmployeeDataById(_id) {
+      let i = 0;
+      let found = false;
+      let employeeToEdit = {};      
+
+      while(i < this.employees.length && found == false) {        
+        if(this.employees[i].id == _id) {
+          found = true;
+        }
+
+        i++;
+      }
+
+      if(found == true) {
+        employeeToEdit = this.employees[i - 1];
+      }
+
+      return employeeToEdit;
+
     },
     deleteSelectedEmployees(ids) {
       console.log(ids);
@@ -154,10 +264,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.add-employee-modal form{
+.employee-modal form{
   text-align: left;
 }
-.add-employee-modal form input[type="text"]{
+.employee-modal form input[type="text"]{
   width: 100%;
   height: 20px;
 }
